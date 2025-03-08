f5bfa1dc1311f90d20fe38682023c8f33f132076
Satyam kumar <satyamkumar2716@gmail.com>
2025-03-08 19:54:01 +0530

    name change

--- src/config/site.ts
+++ src/config/site.ts
@@ -30,9 +30,14 @@ export const siteConfig = {
       label: "Services", 
       // href: "/services", 
       dropdown: [
-        { label: "Web Development", href: "/services/web-development" },
-        { label: "App Development", href: "/services/app-development" },
-        { label: "SEO Optimization", href: "/services/seo-optimization" }
+        { label: "All Services", href: "/listing" },
+        { label: "Teasers in Parties", href: "/services/parties" },
+        { label: "Coffee with Teasers", href: "/services/coffee" },
+        { label: "Teasers for Cheers", href: "/services/cheers" },
+        { label: "Teasers for Advertising", href: "/services/advertising" },
+        { label: "Teasers for Models", href: "/services/models" },
+        { label: "Outgoing with Teasers", href: "/services/outgoing" },
+        { label: "Teasers for Friendship", href: "/services/friendship" }
       ]
     },
     { label: "New Updates", href: "/new-updates" },
